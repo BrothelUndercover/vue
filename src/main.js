9499be3965bf6aff05f780e9b50f0d8dce6cd253
undercover <ashaohero@gmail.com>
2019-01-20 11:42:14 +0800

    git github pages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 引入vue.js的默认值
 import Vue from 'vue'
+//　引入App.vue的默认值
 import App from './App'
-
+//设置false以阻止vue在启用时生成生产提示
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
